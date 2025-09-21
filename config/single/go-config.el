f0e35ece2b05b9877214923822cebabbb5ded5b6
Alessandro Atanasio <a.atanasio@elen.it>
2025-09-21 11:06:47 +0200

    Add base conf for golang scripting

--- config/single/go-config.el
+++ config/single/go-config.el
@@ -1,5 +1,19 @@
-;;; go-config.el -- single configuration
+;;; go-config.el -- single configuration for golang
 
 (use-package go-mode
   :ensure t
-  )
+  :hook ((go-mode . eglot-ensure)
+         (before-save . gofmt-before-save))
+  :config
+  (setq gofmt-command "goimports"))
+
+(with-eval-after-load 'eglot
+  ;; Server per Go
+  (add-to-list 'eglot-server-programs
+               '(go-mode . ("gopls")))
+  
+  ;; Opzioni per gopls (opzionale)
+  (setq-default eglot-workspace-configuration
+                '((gopls
+                   (staticcheck . t)
+                   (matcher . "CaseSensitive")))))
