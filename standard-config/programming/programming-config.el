d6ef25703892140431951ce717b2b9e19dca1eeb
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2025-11-16 15:00:06 +0100

    wip: c mode

--- standard-config/programming/programming-config.el
+++ standard-config/programming/programming-config.el
@@ -1,4 +1,11 @@
+
+
 (require 'eglot-config)
 (require 'corfu-config)
+(require 'c-config)
+(require 'treesit-config)
+
+(require 'programming-integrations)
 
 (provide 'programming-config)
+
---
+++ standard-config/programming/programming-integrations.el
@@ -0,0 +1,25 @@
+
+;; (with-eval-after-load 'eglot
+;;   (add-to-list 'eglot-server-programs
+;;                '((c++-ts-mode c-ts-mode) . ("clangd" "--compile-commands-dir=build-arm"))))
+
+;; Hook per C++
+(defun my-cpp-mode-hook ()
+  "Hook personalizzato per C++"
+  (eglot-ensure)
+  (electric-pair-mode 1)
+  ;(my-cpp-eglot-config)
+
+  ;; clang format when save c++ file
+  (add-hook 'before-save-hook  #'clang-format-buffer nil t)
+  
+  ;; Keybindings specifici per C++
+  (local-set-key (kbd "C-c C-c") 'compile)
+  (local-set-key (kbd "C-c C-r") 'recompile)
+  (local-set-key (kbd "C-c m") 'man))
+
+
+(add-hook 'c++-ts-mode-hook 'my-cpp-mode-hook)
+(add-hook 'c-ts-mode-hook 'my-cpp-mode-hook)
+
+(provide 'programming-integrations)
