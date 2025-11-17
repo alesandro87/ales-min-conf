4c1ec642e12ee7221cc0077803ebaa6df3389f0c
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2025-11-17 09:54:42 +0100

    added bb-mode

--- standard-config/standard-config.el
+++ standard-config/standard-config.el
@@ -3,6 +3,7 @@
 (add-to-list 'load-path (expand-file-name "./ales-min-conf/standard-config/single" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./ales-min-conf/standard-config/programming" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "./ales-min-conf/standard-config/utils" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "./ales-min-conf/standard-config/mode" user-emacs-directory))
 
 (require 'ales-config) ;; load ales configuration, custom shortcut and other simple thinks
 
@@ -10,7 +11,9 @@
     (require 'linux))
     
 (require 'utils-config) ;; load vertico orderless and marginalia
-(require 'programming-config) 
+(require 'programming-config)
+
+(require 'bb-mode)
 
 
 (provide 'standard-config)
