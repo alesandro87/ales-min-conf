6b14a52b7eed070bf74b3ad2205f9030f0d87c7e
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-02-24 11:03:11 +0100

    added shortcut to multi-vterm

--- standard-config/utils/multi-vterm-config.el
+++ standard-config/utils/multi-vterm-config.el
@@ -1,4 +1,13 @@
 (use-package multi-vterm
-  :ensure t)
+  :ensure t
+  :bind (("C-c v t" . multi-vterm)
+         ("C-c v n" . multi-vterm-next)
+         ("C-c v p" . multi-vterm-prev)
+         ("C-c v d" . multi-vterm-dedicated-toggle)
+         ("C-c v o" . multi-vterm-dedicated-open)
+         ("C-c v c" . multi-vterm-dedicated-close)
+         ("C-c v r" . multi-vterm-project))   ;; r = root del progetto
+  :custom
+  (multi-vterm-dedicated-window-height 30))
 
 (provide 'multi-vterm-config)
