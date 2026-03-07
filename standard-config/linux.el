d9f8c4b37aef2aee932ebc9c56a4dd8c3b0b6f70
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-03-07 13:23:08 +0100

    (linux) adapt font size dynamically based on monitor DPI, handle nil display when emacs start as systemd daemon

--- standard-config/linux.el
+++ standard-config/linux.el
@@ -7,10 +7,36 @@
   (exec-path-from-shell-initialize))
 
 ;; font per linux
-(set-face-attribute 'default nil 
-                    :family "Fira Code"
-                    ;;:family "DejaVu Sans Mono"
-                    :height 110)
+;; (set-face-attribute 'default nil 
+;;                     :family "Fira Code"
+;;                     ;;:family "DejaVu Sans Mono"
+;;                     :height 110)
+
+;; Font adattivo ai DPI del monitor
+(defun my/font-size-from-display ()
+  "Calcola font size ottimale dai DPI reali del display."
+  (let* ((attrs (car (display-monitor-attributes-list)))
+         (geom  (alist-get 'geometry attrs))
+         (mm    (alist-get 'mm-size attrs))
+         (dpi   (when (and geom mm
+                           (car mm)
+                           (> (car mm) 0))
+                  (round (/ (* (caddr geom) 25.4)
+                            (car mm))))))
+    (if dpi
+        (max 90 (round (* dpi 1.0)))
+      100)))
+
+(defun my/apply-font (&optional frame)
+  (when (display-graphic-p)
+    (set-face-attribute 'default nil
+                        :family "Hack"
+                        :height (my/font-size-from-display))))
+
+(my/apply-font)
+(add-hook 'after-make-frame-functions #'my/apply-font)
+(add-hook 'window-size-change-functions (lambda (_) (my/apply-font)))
+
 ;;
 (setq org-agenda-files '("/home/ales/Work/Note"))
 
