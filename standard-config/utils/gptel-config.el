67e28ae9ad6bb93efe5db7cf5efbbd91f155ef1d
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-03-31 17:56:09 +0200

    set default claude

--- standard-config/utils/gptel-config.el
+++ standard-config/utils/gptel-config.el
@@ -8,7 +8,7 @@
                     :stream t
                     :models '(qwen2.5-coder:7b)))
          ;; ↓ cambia qui: claude o ollama
-         (default-backend ollama)
+         (default-backend claude)
          (default-model   'qwen2.5-coder:7b))
     (setq gptel-backend default-backend
           gptel-model   default-model)))
