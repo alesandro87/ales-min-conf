58fa405656105335ff29e73989f5ba2f616c6525
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-04-04 12:33:09 +0200

    wip: porjectile

--- standard-config/utils/projectile-config.el
+++ standard-config/utils/projectile-config.el
@@ -1,5 +1,17 @@
 ;;; projectile-config.el --- Project management with Projectile -*- lexical-binding: t; -*-
 
+(defun projectile-consult-find ()
+  "Run `consult-find' from the current Projectile project root."
+  (interactive)
+  (let ((default-directory (projectile-project-root)))
+    (consult-find default-directory)))
+
+(defun projectile-consult-ripgrep ()
+  "Run `consult-ripgrep' from the current Projectile project root."
+  (interactive)
+  (let ((default-directory (projectile-project-root)))
+    (consult-ripgrep default-directory)))
+
 (use-package projectile
   :ensure t
   :init
@@ -7,10 +19,13 @@
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :custom
+  ;; Keep `completing-read' so Vertico/Orderless enhance Projectile too.
   (projectile-completion-system 'default)
   (projectile-sort-order 'recentf)
   (projectile-enable-caching t)
   :config
-  (setq projectile-switch-project-action #'projectile-dired))
+  (setq projectile-switch-project-action #'projectile-dired)
+  (define-key projectile-command-map (kbd "C-f") #'projectile-consult-find)
+  (define-key projectile-command-map (kbd "C-s") #'projectile-consult-ripgrep))
 
 (provide 'projectile-config)
