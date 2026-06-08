ff4ea93b217b3f0b87d375b4ce04385e7f7c00ca
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-06-08 10:04:10 +0200

    added imenulist shortcut

--- standard-config/utils/imenu-list-config.el
+++ standard-config/utils/imenu-list-config.el
@@ -1,9 +1,10 @@
 (use-package imenu-list
   :ensure t
-  :bind (("C-'"
-          . imenu-list-smart-toggle))
+  :bind (("M-0" . imenu-list-smart-toggle))
   :config
   (setq imenu-list-focus-after-activation t
         imenu-list-auto-resize t))
 
 (provide 'imenu-list-config)
+
+
