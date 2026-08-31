e4f0b3cdf91eafeb5bcd3e66534903c8d23c8ad5
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-08-31 11:25:58 +0200

    fix: dart grammar installation

--- standard-config/programming/dart-config.el
+++ standard-config/programming/dart-config.el
@@ -1,11 +1,23 @@
 ;; dart-config.el
 (when (treesit-available-p)
-  (setf (alist-get 'dart treesit-language-source-alist nil nil #'equal)
-        '("https://github.com/UserNobody14/tree-sitter-dart" "c8e7cbb"))
+  ;; `treesit-language-source-alist' non supporta un hash di commit come
+  ;; REVISION quando URL è un repository remoto
+  (defvar ap/dart-grammar-src
+    (expand-file-name "tree-sitter-src/tree-sitter-dart" user-emacs-directory)
+    "Clone locale di tree-sitter-dart, usato per pinnare un commit via hash.")
 
-  ;; Installa la grammatica dart se mancante (hash pinnato per evitare
-  ;; il mismatch ABI 15 vs 14 supportato da Emacs)
-  (unless (treesit-language-available-p 'dart)
+  (defun ap/dart-install-grammar ()
+    "Installa la grammatica tree-sitter per Dart, pinnata al commit
+c8e7cbb (ABI 14) per evitare il version-mismatch con Emacs.  Da
+lanciare a mano una tantum quando serve: M-x ap/dart-install-grammar."
+    (interactive)
+    (unless (file-directory-p ap/dart-grammar-src)
+      (make-directory (file-name-directory ap/dart-grammar-src) t)
+      (call-process "git" nil nil nil "clone" "--quiet"
+                    "https://github.com/UserNobody14/tree-sitter-dart"
+                    ap/dart-grammar-src))
+    (setf (alist-get 'dart treesit-language-source-alist nil nil #'equal)
+          (list ap/dart-grammar-src "c8e7cbb"))
     (treesit-install-language-grammar 'dart)))
 
 (defun ap/dart-ts-setup ()
