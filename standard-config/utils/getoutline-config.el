1dbf2895a996e6442af33307525e11d4bd54e925
Alessandro Atanasio <alessandro.atanasio@gmail.com>
2026-09-15 18:38:35 +0200

    fix outline

--- standard-config/utils/getoutline-config.el
+++ standard-config/utils/getoutline-config.el
@@ -5,7 +5,7 @@
 
 ;; Sviluppo locale: metti a `t` mentre editi il pacchetto in place.
 ;; Metti a `nil` per installarlo dal repo remoto via :vc.
-(defvar getoutline-use-local-path nil)
+(defvar getoutline-use-local-path t)
 
 (if getoutline-use-local-path
     (use-package getoutline
@@ -15,13 +15,13 @@
       (setq getoutline-url "http://192.168.0.99:4000"   ; il tuo Outline
             getoutline-api-token (getenv "OUTLINE_API_TOKEN")))
 
-  ;; else 
-  (use-package getoutline
-    :vc (:url "git@github.com:alesandro87/getoutline.git"
-              :branch dev
-              :rev :newest)
-    :config
-    (setq getoutline-url "http://192.168.0.99:4000"
-          getoutline-api-token (getenv "OUTLINE_API_TOKEN"))))
+(use-package getoutline
+  :vc (:url "git@github.com:alesandro87/getoutline.git"
+            :branch dev
+            :rev :newest)
+  :config
+  (setq getoutline-url "http://192.168.0.99:4000"   ; il tuo Outline
+        getoutline-api-token (getenv "OUTLINE_API_TOKEN")
+        getoutline-curl-options '("--insecure"))))
 
 (provide 'getoutline-config)
